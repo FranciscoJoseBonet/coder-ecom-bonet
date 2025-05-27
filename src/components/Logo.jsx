562ce3eaf51178ddbcf7e7c927cbd75b3083616a
FranciscoJoseBonet Desktop <franciscojosebonet@gmail.com>
2025-05-26 21:31:13 -0300

    Correccion de link en el logo de la pagina

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Logo = () => {
 	return (
-		<Link className="navbar-brand d-flex align-items-center" href="/">
+		<Link className="navbar-brand d-flex align-items-center" to="/">
 			<FaGuitar className="me-2" />
 			<span className="fw-bold">RbMusic</span>
 		</Link>
